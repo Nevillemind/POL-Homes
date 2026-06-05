e605ba45c22ff67f37e2724c6c8cd73cb960b630
George Mundin <georgemundin@Mac.fios-router.home>
2026-06-05 08:33:37 -0400

    Add Resident PWA, update website with Apply Now CTA + Resident Portal link

---
+++ resident-app/sw.js
@@ -0,0 +1,18 @@
+const CACHE='pol-homes-v1';
+const ASSETS=['/','/index.html','/manifest.json'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+      if(res.ok){
+        const clone=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,clone));
+      }
+      return res;
+    }).catch(()=>caches.match('/index.html')))
+  );
+});
